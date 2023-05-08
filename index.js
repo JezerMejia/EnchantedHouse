177f7de9d84da980386f6b49ad467150ff84f51f
Jezer Mejía <amyuki4@icloud.com>
2023-05-08 00:52:57 -0600

    Added some logic and animations

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const thunderbolt = document.querySelector("#thunderbolt");
   const thunderSound = document.querySelector("#thunderSound");
   const sky = document.querySelector("#sky");
+  const ghostEntity = document.querySelector("#ghostEntity");
+  const daniEntity = document.querySelector("#daniEntity");
 
   setInterval(() => {
     thunderbolt.emit("startanim", null, false);
@@ -13,4 +15,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
     thunderSound.components.sound.playSound();
   }, 15000);
+
+  setInterval(() => {
+    ghostEntity.emit("startanim", null, false);
+    ghostEntity.emit("endanim", null, false);
+
+    daniEntity.emit("startanim", null, false);
+    daniEntity.emit("endanim", null, false);
+  }, 30000);
+});
+
+AFRAME.registerComponent("model-relative-opacity", {
+  schema: { default: 1.0, type: "number" },
+  init: function () {
+    this.nodeMap = {};
+    this.prepareMap.bind(this);
+    this.traverseMesh.bind(this);
+
+    this.el.addEventListener("model-loaded", (e) => {
+      this.prepareMap();
+      this.update();
+    });
+  },
+  prepareMap: function () {
+    this.traverseMesh((node) => {
+      this.nodeMap[node.uuid] = node.material.opacity;
+    });
+  },
+  update: function () {
+    this.traverseMesh((node) => {
+      node.material.opacity = this.nodeMap[node.uuid] * this.data;
+      node.material.transparent = node.material.opacity < 1.0;
+      node.material.needsUpdate = true;
+    });
+  },
+  remove: function () {
+    this.traverseMesh((node) => {
+      node.material.opacity = this.nodeMap[node.uuid];
+      node.material.transparent = node.material.opacity < 1.0;
+      node.material.needsUpdate = true;
+    });
+  },
+  traverseMesh: function (func) {
+    var mesh = this.el.getObject3D("mesh");
+    if (!mesh) {
+      return;
+    }
+    mesh.traverse((node) => {
+      if (node.isMesh) {
+        func(node);
+      }
+    });
+  },
 });
